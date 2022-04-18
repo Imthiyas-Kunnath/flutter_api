e9177da90b710854a486f9d738dc2ee7b7a7c726
Imthiyas-Kunnath <imthiyaskunnath@outlook.com>
2022-04-19 01:21:17 +0530

    Listview design

--- lib/views/homepage.dart
+++ lib/views/homepage.dart
@@ -39,7 +39,41 @@ class _HomePageState extends State<HomePage> {
             itemCount: posts?.length,
             itemBuilder: (context, index){
           return Container(
-            child: Text(posts![index].title),
+            padding: const EdgeInsets.all(16),
+            child: Row(
+              children: [
+                Container(
+                  height: 50,
+                  width: 50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                    color: Colors.grey[300],
+                  ),
+                ),
+                SizedBox(width: 16),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                          posts![index].title,
+                          maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold
+                        ),
+                      ),
+                      Text(
+                        posts![index].body ?? '',
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           );
         }),
         replacement: const Center(
